acaee1fb1f5ff8c6e9ee71208451af0b17386a92
pandeyl <lakshmipandey7@gmail.com>
2020-06-16 17:30:36 +0530

    directory page

---
+++ Object Repository/MyWedding_Objects/MyWedding_Desktop/directorypagealphabetlist.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>directorypagealphabetlist</name>
+   <tag></tag>
+   <elementGuidId>1c188f04-4027-4ad8-a6ae-0f9aff854e66</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='index clearfix']//li</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
