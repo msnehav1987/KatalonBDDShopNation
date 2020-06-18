bafdbb75771ba086af43ebae6cd9c6b04890309d
pandeyl <lakshmipandey7@gmail.com>
2020-06-18 18:18:40 +0530

    commit

---
+++ Object Repository/IntegratedTestObjects/RealSimplePDPHeroImage.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>RealSimplePDPHeroImage</name>
+   <tag></tag>
+   <elementGuidId>98cccef8-fac2-4045-a456-21621c8d8cac</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='product-detail__photo col-xs-12 col-sm-6']//a/img</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
